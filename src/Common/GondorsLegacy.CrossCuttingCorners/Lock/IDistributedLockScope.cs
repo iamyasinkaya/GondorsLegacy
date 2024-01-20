16bac95610098928beed494d93cb647a795d5409
YASIN KAYA <yasin.kaya@tatilbudur.com>
2024-01-20 19:22:47 +0300

    Lock has been added to the project.

---
+++ src/Common/GondorsLegacy.CrossCuttingCorners/Lock/IDistributedLockScope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GondorsLegacy.CrossCuttingCorners.Lock;
+
+/// <summary>
+/// Dağıtılmış kilit alanının (scope) kullanımını temsil eden arayüz.
+/// </summary>
+public interface IDistributedLockScope : IDisposable
+{
+    /// <summary>
+    /// Kilit alanının hala elde tutulup tutulmadığını kontrol eder.
+    /// </summary>
+    /// <returns>Kilit alanı hala elde tutuluyorsa true, aksi halde false.</returns>
+    bool StillHoldingLock();
+}
